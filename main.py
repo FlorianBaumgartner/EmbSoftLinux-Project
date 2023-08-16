7283035ae222a30fd66278947bcc6c04f78e9237
Florian Baumgartner <florian.b-h@bluewin.ch>
2023-08-16 15:44:26 +0200

    Added Main

---
+++ main.py
@@ -0,0 +1,22 @@
+from PseudoGpt import PseudoGpt
+from stt4sg import Stt4Sg
+from pathlib import Path
+
+
+class Main:
+    def __init__(self):
+        self.stt4sg = Stt4Sg()
+        self.gpt = PseudoGpt()
+
+
+    def test1(self):
+        file = Path(__file__).parent / "test.mp3"
+        transcript = self.stt4sg.getTranscript(file)
+        prompt = f"Fasse mir den folgenden Text zusammen (auf Deutsch): {transcript}"
+        answer = self.gpt.generate(prompt)
+        print(answer)
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.test1()
